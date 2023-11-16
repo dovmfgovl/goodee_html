919ce5d69a0416761bcbb79290211fa0ef78cac0
dovmfgovl <plum0626@daum.net>
2023-11-16 18:37:35 +0900

    myBook 게시판 작성,리스트,조회 구현

--- vscode_express/myBook/routes/index.js
+++ vscode_express/myBook/routes/index.js
@@ -13,13 +13,13 @@ router.get('/', function(req, res, next) {
 router.get('/login', function(req, res, next) { //app.js -> path 라이브러리 __dirname, views
   res.render('index', { title: '로그인', pageName:"auth/login.ejs" });
 });
-router.get('/board/list', function(req, res, next) { //app.js -> path 라이브러리 __dirname, views
+router.get('/board', function(req, res, next) { //app.js -> path 라이브러리 __dirname, views
   res.render('index', { title: '글목록', pageName:"board/list.ejs" });
 });
 router.get('/board/write', function(req, res, next) { //app.js -> path 라이브러리 __dirname, views
   res.render('index', { title: '글쓰기', pageName:"board/write.ejs" });
 });
-router.get('/board/read/:id', function(req, res, next) { //app.js -> path 라이브러리 __dirname, views
+router.get('/board/:id', function(req, res, next) { //app.js -> path 라이브러리 __dirname, views
   let id = req.params.id;
   res.render('index', { title: '상세보기', pageName:"board/read.ejs", id: id });
 });
